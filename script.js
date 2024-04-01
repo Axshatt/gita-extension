3b0328f057ceea82603d684333dcacc6c8a278ee
Akshat Singh <codepreneur.in@gmail.com>
2024-04-01 12:45:56 +0530

    added navigation for chapters and updated logos

--- script.js
+++ script.js
@@ -1,32 +1,25 @@
 let currpg = 1;
 
-// document.getElementById("previous").addEventListener("onclick", function () {
-//   if (currpg > 1) {
-// }
-// currpg--;
-// });
-// document.getElementById("next").addEventListener("click", function () {
-//   if (currpg < 18) {
-//     return currpg++;
-//   }
-// });
 document.getElementById("previous").addEventListener("click", previous);
 document.getElementById("next").addEventListener("click", next);
+
 
 
 function previous() {
   if (currpg > 1) {
     currpg--;
   }
+  getData();
 }
 
 function next() {
   if (currpg < 18) {
     currpg++;
   }
+  getData();
 }
 
-console.log(currpg);
+
 
 async function getData() {
   const response = await fetch(`https://bhagavadgitaapi.in/chapter/${currpg}/`);
@@ -34,6 +27,7 @@ async function getData() {
   const data = await response.json();
   const summarytxt = data.summary.en;
   const chNo = data.chapter_number;
+  
 
   const summary = document.getElementById("summary");
 
@@ -42,6 +36,8 @@ async function getData() {
   const ch = document.getElementById("ch");
 
   ch.innerHTML = "Chapter " + chNo;
+
+  document.getElementById("loader").style.display="none";
 }
 
 getData();
